7f4e45f9df74bb432cf2b1f21fa80c232663f479
NotEnoughCoffee <andrewlsparks@gmail.com>
2025-05-08 17:44:04 -0400

    GUI feature V2 complete -> GUI now displays categories and all related results vertically. interface is now functional again

--- src/dev/apg/gui/DisplayUI.java
+++ src/dev/apg/gui/DisplayUI.java
@@ -9,7 +9,6 @@ import dev.apg.utility.FormatText;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class DisplayUI extends FileLoader {
@@ -18,7 +17,7 @@ public class DisplayUI extends FileLoader {
     public static String promptButtonText;
     public String uiText;
     public int x, y;
-    private final Rectangle uiDimensions = new Rectangle(80, 212, 640, 220);
+    private final Rectangle resultsDisplayDimensions = new Rectangle(80, 212, 640, 220);
     public static List<Selection> currentRollMemory;
 
     //GUI SETUP//
@@ -59,43 +58,17 @@ public class DisplayUI extends FileLoader {
         drawPromptButtonText();
         setResultsDisplay(currentRollMemory);
 
-        //OTHER NOTES://
-        //alter challenge roll methods to min 1, max 5 categories (if it's a repeat don't add to the List) + the method that takes List<Category> will roll each category in the list 1 time, then if there's a difference in requested roll count and rolls already made, then it randomly rolls categories from the list (or a different handler method to deal with EXACT rolls to be made if specified in the saved ChallengeList details) (AND Maybe a limit on overall rolls per category)
-
-        //make a music mute button that moves away from cursor when in a range
-        //            //
-
-        //clean up above
-        //To draw the text where categories are displayed at the top and the selection below:
-
-
-        // First I need to build a List<Category> of all the rolled challenges in the List<Selection>
-        // I will use that to find any duplicates, and log the duplicates somehow
-        // this will allow me to combine multiple results of the same category when displayed
-
-        //then I need a sub list of unique category names and a count
-        // the count will be used to determine how the screen is split
-
-
-        /// screenSplit could be a method that returns a rectangle[] based on the category count that uses math to determine width and a loop.
-        // a loop of for(int i = 0; i < rect.size? ; i++) ->
-        // Display a randomized polygon graphics based on 2 subsections of rectangle[i]. make a polygon that picks points roughly in subsections of the subsection rectangle, and makes a 4 sided shape from that (to be weird and funky?)
-        // -> later, this is lower priority and its own method
-        // Display List<Cat>[i].name; centered in a defined subsection of rectangle[i]; (using dimensions of text to determine if text size needs to be shrunk or wrapped to fit in space (detect " ") ) (own method)
-        // THEN choose each selection in List<selection> that has matching .category() and display each on their own line, in the lower subsection of the rectangle[i] using width detection of text (so make its own method) to determine font size and word wrap if spaces. (or pick the widest word to determine width?) using its own method that takes the matching selections as a (Selection... selections) parameter and getting its  count to determine the below splitting:
-        // if multiple selections, split into string[]; and use a loop to divide the subsection for each result (and changing display graphic to show a line between choices)
-        // may have to divide differently if 4+ selections in a 1-2 category challenge or just limit to 4 per category max
 
 
         //REMOVE//
         //Below is a temporary solution to display roll text until challenge class changes are implemented
-        String[] text = uiText.split(";;");
-        y = 150 + (350 / 2) - ((text.length - 1) * 39) / 2;
-        for (String s : text) {
-            getFontCenterX(s);
-            g2D.drawString(s, x, y); //use this to draw multiple strings with a loop
-            y += 40;
-        }
+//        String[] text = uiText.split(";;");
+//        y = 150 + (350 / 2) - ((text.length - 1) * 39) / 2;
+//        for (String s : text) {
+//            getFontCenterX(s);
+//            g2D.drawString(s, x, y); //use this to draw multiple strings with a loop
+//            y += 40;
+//        }
         //REMOVE//
     }
 
@@ -111,22 +84,38 @@ public class DisplayUI extends FileLoader {
     }
 
     public void setResultsDisplay(List<Selection> selections) {
-        g2D.setColor(Color.white);
+        g2D.setColor(Color.white); //sets color to white for all 'Results' and 'Category Names'
         List<Category> categories = getUniqueCategories(selections);
-        int count = categories.size();
-        Rectangle[] displayAreas = splitScreen(count);
+        int categoryCount = categories.size();
+        Rectangle[] displayAreas = splitScreen(categoryCount);
 
-        for (int i = 0; i < count; i++) {
-            //Draw display area polygon -> implement later
+
+        for (int i = 0; i < categoryCount; i++) {
+            //drawPolygon -> method to implement later to draw polygon based off of Rectangle Dimensions
             String catName = categories.get(i).name;
-            drawTextInDisplayArea(catName, displayAreas[i]);
-            //Draw Category centered and sized text using rectangle dimensions.
-            //change rectangle y = y + yHeight; yHeight = uiDimensions - yHeight;
-            // that should return the dimensions of the text display area for selection
-            // -> then split that into a sub rect array based on number of selections in the current category
+            drawTextInDisplayArea(catName, displayAreas[i]); //draws the text fed into this method split and centered in specified area
 
-            //now draw selection text centered and sized using the new dimensions via another loop AND DONE
-            //
+            List<String> resultsInCategory = new ArrayList<>();
+            selections.forEach( s -> {
+                if(s.Category().equalsIgnoreCase(catName)){
+                    resultsInCategory.add(s.name());
+                }
+            });
+            int resultCount = resultsInCategory.size();
+            int subDisplayCounter = i + categoryCount;
+            //second half of displayArea is now the results display area (so displayAreas[i + categoryCount])
+            //create a sub space and loop through displaying each result
+
+            Rectangle subDisplay = new Rectangle(
+                    displayAreas[subDisplayCounter].x,
+                    displayAreas[subDisplayCounter].y,
+                    displayAreas[subDisplayCounter].width,
+                    displayAreas[subDisplayCounter].height/resultCount);
+
+            for(String line : resultsInCategory) {
+                drawTextInDisplayArea(line,subDisplay);
+                subDisplay.y += subDisplay.height;
+            }
         }
 
     }
@@ -169,7 +158,7 @@ public class DisplayUI extends FileLoader {
         int pixelHeight, pixelWidth;
 
         int fontSize = 50; //largest font size to test
-        int multiLineFontSize = 50; //largest font size for multiLine to test
+        int multiLineFontSize = 38; //largest font size for multiLine to test
 
         int lineCount = 1; //keeps track of how many lines to split
 
@@ -180,10 +169,6 @@ public class DisplayUI extends FileLoader {
 
 
         boolean[] textPass = {false, false}; // { [0] WHILE LOOP PASS FLAG // [1] MULTILINE SPLIT FLAG }
-
-        // REMOVE:
-        //StringBuilder currentLineText = new StringBuilder();
-        //int multiLinePixelHeight = ((multiLineFontSize * 2) / 3 * 4) + 2;
 
         while (!textPass[0]) {
             currentFont = new Font("Broadway", Font.PLAIN, (textPass[1] ? multiLineFontSize : fontSize)); //sets the current font size using updated size from previous loop
@@ -208,38 +193,36 @@ public class DisplayUI extends FileLoader {
 
             //MULTILINE TRIGGERED// -> multiple words detected in the displayText and lower bound of 32pt reached. Text will now be split to multiple lines:
 
-                if (words.length == 2 && textPass[1]) { //Checks word count if there are only two words,
-                    balancedLine = Arrays.asList(words);
-                    widestWord = getWidestLine(null, balancedLine);
-                    pixelHeight = getPixelHeightOfWord(widestWord, currentFont);
-                    pixelWidth = getPixelWidthOfWord(widestWord, currentFont);
-                    lineCount = 2;
-                    textPass = textWidthCheck(pixelHeight, pixelWidth, displayWidth, displayHeight, widestWord, currentFont);
-                    if (!textPass[0]) {
-                        multiLineFontSize--;
-                    }
-                } else if (words.length >= 3 && textPass[1]) {
+//                if (words.length == 2 && textPass[1]) { //Checks word count if there are only two words,
+//                    balancedLine = Arrays.asList(words);
+//                    widestWord = getWidestLine(null, balancedLine);
+//                    pixelHeight = getPixelHeightOfWord(widestWord, currentFont);
+//                    pixelWidth = getPixelWidthOfWord(widestWord, currentFont);
+//                    lineCount = 2;
+//                    textPass = textWidthCheck(pixelHeight, pixelWidth, displayWidth, displayHeight, widestWord, currentFont);
+//                    if (!textPass[0]) {
+//                        multiLineFontSize--;
+//                    }
+//                } else
+                    if (words.length >= 2 && textPass[1]) {
                     if(lineCount < 2) { //makes sure there's at least two lines
                         lineCount = 2;
                     }
                     balancedLine = FormatText.balanceMultiline(displayText,lineCount); //updates the balanced line with the displayText split evenly across lineCount # of lines.
                     widestWord = getWidestLine(null,balancedLine);
                     pixelWidth = getPixelWidthOfWord(widestWord,currentFont);
-                    pixelHeight = (getPixelHeightOfWord(widestWord,currentFont) + 2) * lineCount; //height of lines + 2px spacing
+                    pixelHeight = getPixelHeightOfWord(widestWord,currentFont) * lineCount; //height of all lines
                     textPass = textWidthCheck(pixelHeight, pixelWidth, displayWidth, displayHeight, widestWord, currentFont);
-                    //if the ext passes here, the rest of the conditions below will be skipped.
                     if(!textPass[0]) { //if text does not pass, size is decreased.
                         multiLineFontSize--;
-                        if(words.length != lineCount) { // if not already at max lines per words
-                            //split to more lines
-                            if (multiLineFontSize == 32 && lineCount == 2) {
+                        if(words.length != lineCount) { // if not already at max lines per words -> split more lines
+                            if (multiLineFontSize == 31 && lineCount == 2) {
                                 multiLineFontSize = 42;
                                 lineCount++;
-                            }else if(multiLineFontSize == 28 && lineCount == 3) {
+                            }else if(multiLineFontSize == 27 && lineCount == 3) {
                                 multiLineFontSize = 38;
                                 lineCount++;
-                            }else if(multiLineFontSize == 24 && lineCount >= 4) {
-                                //with 4+ lines, increment lines every time text gets down to 24
+                            }else if(multiLineFontSize == 23 && lineCount >= 4) { //with 4+ lines, increment lines every time text gets down to 24
                                 multiLineFontSize = 36;
                                 lineCount++;
                             }
@@ -247,48 +230,54 @@ public class DisplayUI extends FileLoader {
                     }
                 }
         }
-        //once out of loop
-        //currentFont will be the proper text size
-        //lineCount will keep track of current number of lines.
-        //balancedLine will contain all lines of text to be displayed, on however many lines it takes to fit box at currentFont size.
-
-        //Text can now be output to be displayed
-        drawTextInDisplayArea(balancedLine,currentFont,displayArea);
-
+        drawTextInDisplayArea(balancedLine,currentFont,displayArea); //Text drawn
     }
 
     private void drawTextInDisplayArea (List<String> displayText, Font currentFont, Rectangle displayArea) {
         setFont(g2D, currentFont);
+        //takes a List and Font and draws each line as a text box centered in the displayArea's bounds based.
+        int centeredX;
+        int centeredY = (displayArea.height/2 + displayArea.y) + ((displayText.size() * getPixelHeightOfWord("EXAMPLE", currentFont)) / 4) - (getPixelHeightOfWord("EXAMPLE",currentFont) * (displayText.size() - 1))/4*3;
 
+        for(String text : displayText) {
+            centeredX = (displayArea.x + displayArea.width/2) - getPixelWidthOfWord(text,currentFont)/2;
+            g2D.drawString(text,centeredX,centeredY);
 
+            g2D.draw(displayArea);
+            centeredY += getPixelHeightOfWord(text,currentFont);
+        }
     }
 
 
     private boolean[] textWidthCheck(int pixelHeight, int pixelWidth, int displayWidth, int displayHeight, String displayText, Font currentFont) {
+        //checks if displayText fits in displayArea based on currentFont -> which is incremented each time this method is run inside a loop
         boolean[] textPass = {false, false};
         int fontSize = currentFont.getSize();
-        if (fontSize > 32 &&
-                (pixelWidth > (displayWidth - 4) ||
-                        pixelHeight > (displayHeight - 4))) {
+        int innerHeight = 15;
+        int innerWidth = 20;
+        if (fontSize > 20 &&
+                (pixelWidth > (displayWidth - innerWidth) ||
+                        pixelHeight > (displayHeight - innerHeight))) {
             //Font size too wide or tall, fail and try again
-            return textPass;
-        } else if (pixelWidth <= displayWidth - 4) {
-            //text size fits within the display and starts out at least being >= 32 font size
+            return textPass; // text fails
+        } else if (pixelWidth <= displayWidth - innerWidth && pixelHeight <= displayHeight - innerHeight) {
+            //if the text fits in the display area, pass
             textPass[0] = true;
-            return textPass;
+            return textPass; // pass true
         } else if (!displayText.contains(" ") && fontSize >= 8) {
-            //multiline checked at font size 32 if it can be split, function will return multiline boolean true
-            //keeps reducing font size if Category name does not contain spaces
+            //WidthCheck will keep failing while fontSize is > 8. 8 is the minimum display size, and will be forced despite breaking box bounds
             if (fontSize == 8) {
-                //if font size still not thin enough, the word is too long and text size will be set to 8
-                System.out.println("Issue with Category Name Length, Word too long or no spaces detected, overlapping may occur");
-                textPass[0] = true;
+                System.out.println("Issue with Category Name Length, word too long or no spaces detected, overlapping may occur");
+                textPass[0] = true; // pass is forced
                 return textPass;
             }
-            return textPass;
+            return textPass; // pass fails
         }else {
             textPass[1] = true;
-            return textPass;
+            if (fontSize == 8){ //minimum font size reached
+                textPass[0] = true;
+            }
+            return textPass; // multiline passes because line contains " "
         }
     }
 
@@ -298,14 +287,25 @@ public class DisplayUI extends FileLoader {
     }
 
     private Rectangle[] splitScreen(int count) {
-        //Returns an array of the display subsection dimensions for the category
-        int xPoint = uiDimensions.x;
-        int yPoint = uiDimensions.y;
-        int displayWidth = uiDimensions.width / ((count > 0) ? count : 1);
-        int displayHeight = uiDimensions.height * 36/100;
-        Rectangle[] displayAreas = new Rectangle[count];
+        //Returns an array of the display subsection dimensions for the category, and then the selection areas
+        int displayBorder = 10;
+        int xPoint = resultsDisplayDimensions.x;
+        int yPoint = resultsDisplayDimensions.y;
+        int displayWidth = (resultsDisplayDimensions.width / ((count > 0) ? count : 1)) - (displayBorder); // minimum width set to 1 or count
+
+
+        int displayHeight = resultsDisplayDimensions.height * 36/100; //height set to 36% of display area.
+        Rectangle[] displayAreas = new Rectangle[count * 2]; //doubles count for category + selection
             for (int i = 0; i < count; i++){
-                displayAreas[i] = new Rectangle(xPoint + (i * displayWidth), yPoint, displayWidth, displayHeight);
+                displayAreas[i] = new Rectangle(xPoint + (i * displayWidth) + (i * displayBorder), yPoint, displayWidth, displayHeight);
+                g2D.draw(displayAreas[i]);
+            }
+            //change height and yPoint
+            yPoint = yPoint + displayHeight + displayBorder;
+            displayHeight = resultsDisplayDimensions.height * 64/100 - displayBorder;
+            for(int i = 0; i < count; i++) {
+                displayAreas[i + count] = new Rectangle(xPoint + (i * displayWidth) + (i * displayBorder), yPoint, displayWidth, displayHeight);
+                g2D.draw(displayAreas[i + count]);
             }
         return displayAreas;
     }
